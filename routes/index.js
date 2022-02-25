7d3932dab61d16f6ca1a7879ddea7c09b07b9ac4
Jaime Canales <jaime.canales@beetrack.com>
2022-02-25 16:13:52 -0300

    feat: added routing for uploaders

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ const categoriesRouter = require('./categories')
 const productsRouter = require('./products')
 const searchRouter = require('./products')
 const usersRouter = require('./products')
+const uploadRoutes = require('./uploads')
 
 module.exports = {
   authRouter,
   categoriesRouter,
   productsRouter,
   searchRouter,
-  usersRouter
+  usersRouter,
+  uploadRoutes
 }
